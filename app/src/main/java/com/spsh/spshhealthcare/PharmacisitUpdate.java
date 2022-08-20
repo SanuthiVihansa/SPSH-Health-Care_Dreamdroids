02e105e30d8a4418d0d1899baa32221576c5b375
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-20 19:58:19 +0530

    Initial commit

---
+++ app/src/main/java/com/spsh/spshhealthcare/PharmacisitUpdate.java
@@ -0,0 +1,24 @@
+package com.spsh.spshhealthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class PharmacisitUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_pharmacisit_update);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/spsh/spshhealthcare/PharmisisitSearch.java
@@ -0,0 +1,19 @@
+package com.spsh.spshhealthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class PharmisisitSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_pharmisisit_search);
+    }
+}
