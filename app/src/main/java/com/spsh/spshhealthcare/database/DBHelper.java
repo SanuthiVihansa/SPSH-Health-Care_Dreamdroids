3249a2af45f3accd24ee060a7beb422d56eb711b
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-21 22:57:25 +0530

    Created and modified DBHelper.java file

---
+++ app/src/main/java/com/spsh/spshhealthcare/database/DBHelper.java
@@ -0,0 +1,148 @@
+package com.spsh.spshhealthcare.database;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.spsh.spshhealthcare.R;
+import com.spsh.spshhealthcare.models.Report;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "SPSH.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PATIENT_TABLE =
+                "CREATE TABLE " + PatientsMaster.Patients.TABLE_NAME + " ("+
+                        PatientsMaster.Patients.COLUMN_NAME_NIC + " TEXT PRIMARY KEY," +
+                        PatientsMaster.Patients.COLUMN_NAME_NAME + " TEXT," +
+                        PatientsMaster.Patients.COLUMN_NAME_CONTACTNO + " TEXT," +
+                        PatientsMaster.Patients.COLUMN_NAME_PASSWORD + " TEXT)";
+
+        String SQL_CREATE_REPORT_TABLE =
+                "CREATE TABLE " + ReportsMaster.Reports.TABLE_NAME + " (" +
+                        ReportsMaster.Reports._ID + " INTEGER PRIMARY KEY, " +
+                        ReportsMaster.Reports.COLUMN_NAME_NAME + " TEXT, " +
+                        ReportsMaster.Reports.COLUMN_NAME_AGE + " INTEGER, " +
+                        ReportsMaster.Reports.COLUMN_NAME_GENDER + " TEXT, " +
+                        ReportsMaster.Reports.COLUMN_NAME_NIC + " TEXT, " +
+                        ReportsMaster.Reports.COLUMN_NAME_DATE + " TEXT, " +
+                        ReportsMaster.Reports.COLUMN_NAME_TIME + " TEXT, " +
+                        ReportsMaster.Reports.COLUMN_NAME_COST + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_HEMOGLOBIN + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_WBC + " INTEGER, " +
+                        ReportsMaster.Reports.COLUMN_NAME_NEUTROPHILS + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_LYMPHOCYTES + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_EOSINOPHILS + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_RBC + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_PCB + " REAL, " +
+                        ReportsMaster.Reports.COLUMN_NAME_PLATELET + " INTEGER)";
+
+        db.execSQL(SQL_CREATE_PATIENT_TABLE);
+        db.execSQL(SQL_CREATE_REPORT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public long addToPatients(String name, String nic, String contactNo, String password){
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PatientsMaster.Patients.COLUMN_NAME_NIC, nic);
+        values.put(PatientsMaster.Patients.COLUMN_NAME_NAME, name);
+        values.put(PatientsMaster.Patients.COLUMN_NAME_CONTACTNO, contactNo);
+        values.put(PatientsMaster.Patients.COLUMN_NAME_PASSWORD, password);
+
+        return db.insert(PatientsMaster.Patients.TABLE_NAME, null, values);
+    }
+
+    public List getPatient(String nic){
+        SQLiteDatabase db = getReadableDatabase();
+
+        String[] projection = {
+                PatientsMaster.Patients.COLUMN_NAME_NIC
+        };
+
+        String selection = PatientsMaster.Patients.COLUMN_NAME_NIC + " LIKE ?";
+        String[] stringArgs = {nic};
+
+        Cursor cursor = db.query(
+                PatientsMaster.Patients.TABLE_NAME,
+                projection,
+                selection,
+                stringArgs,
+                null,
+                null,
+                null
+        );
+
+        List patients = new ArrayList();
+
+        while(cursor.moveToNext()){
+            String pNic = cursor.getString(cursor.getColumnIndexOrThrow(PatientsMaster.Patients.COLUMN_NAME_NIC));
+            patients.add(pNic);
+        }
+
+        cursor.close();
+
+        return patients;
+    }
+
+    public long addReport(String name, int age, String gender, String nic, String date, String time, double cost, double hemoglobin, int wbc, double neutrophils, double lymphocytes, double eosinophils, double rbc, double pcb, int platelet){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(ReportsMaster.Reports.COLUMN_NAME_NAME, name);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_AGE, age);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_GENDER, gender);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_NIC, nic);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_DATE, date);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_TIME, time);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_COST, cost);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_HEMOGLOBIN, hemoglobin);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_WBC, wbc);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_NEUTROPHILS, neutrophils);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_LYMPHOCYTES, lymphocytes);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_EOSINOPHILS, eosinophils);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_RBC, rbc);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_PCB, pcb);
+        values.put(ReportsMaster.Reports.COLUMN_NAME_PLATELET, platelet);
+
+        return db.insert(ReportsMaster.Reports.TABLE_NAME, null, values);
+    }
+
+    @SuppressLint("Range")
+    public ArrayList<HashMap<String, String>> getAllReports(){
+        SQLiteDatabase db = getReadableDatabase();
+        ArrayList<HashMap<String, String>> reportList = new ArrayList<>();
+        String query = "SELECT " + ReportsMaster.Reports._ID + ", " + ReportsMaster.Reports.COLUMN_NAME_NAME + ", " + ReportsMaster.Reports.COLUMN_NAME_AGE + ", " + ReportsMaster.Reports.COLUMN_NAME_NIC + " FROM " + ReportsMaster.Reports.TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+
+        while(cursor.moveToNext()){
+            HashMap<String, String> report = new HashMap<>();
+            report.put(ReportsMaster.Reports._ID, cursor.getString(cursor.getColumnIndex(ReportsMaster.Reports._ID)));
+            report.put(ReportsMaster.Reports.COLUMN_NAME_NAME, cursor.getString(cursor.getColumnIndex(ReportsMaster.Reports.COLUMN_NAME_NAME)));
+            report.put(ReportsMaster.Reports.COLUMN_NAME_AGE, cursor.getString(cursor.getColumnIndex(ReportsMaster.Reports.COLUMN_NAME_AGE)));
+            report.put(ReportsMaster.Reports.COLUMN_NAME_NIC, cursor.getString(cursor.getColumnIndex(ReportsMaster.Reports.COLUMN_NAME_NIC)));
+
+            reportList.add(report);
+        }
+
+        return reportList;
+    }
+}
