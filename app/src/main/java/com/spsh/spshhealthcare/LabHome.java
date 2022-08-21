03faca2279c05d270215841b002703c1817aa057
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-21 22:53:20 +0530

    Created and modified LabHome.java file

---
+++ app/src/main/java/com/spsh/spshhealthcare/LabHome.java
@@ -0,0 +1,31 @@
+package com.spsh.spshhealthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class LabHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_lab_home);
+    }
+
+    public void onClickViewAllLabReportBtn(View view){
+        Intent intent = new Intent(this, ViewAllReports.class);
+        startActivity(intent);
+    }
+
+    public void onClickMakeReportBtn(View view){
+        Intent intent = new Intent(this, AddReport.class);
+        startActivity(intent);
+    }
+}
