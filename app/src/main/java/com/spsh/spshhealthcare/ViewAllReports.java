8dc986bc62453c38f71361d5e836e2b6f04b82a9
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-24 20:47:21 +0530

    Modified ViewAllReports.java file

--- app/src/main/java/com/spsh/spshhealthcare/ViewAllReports.java
+++ app/src/main/java/com/spsh/spshhealthcare/ViewAllReports.java
@@ -1,24 +1,23 @@
 package com.spsh.spshhealthcare;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.spsh.spshhealthcare.database.DBHelper;
-import com.spsh.spshhealthcare.models.Report;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class ViewAllReports extends AppCompatActivity {
 
@@ -34,7 +33,22 @@ public class ViewAllReports extends AppCompatActivity {
         ArrayList<HashMap<String, String>> reportList = dbHelper.getAllReports();
 
         ListView lv_reportList = findViewById(R.id.lv_reportList);
-        ListAdapter adapter = new SimpleAdapter(ViewAllReports.this, reportList, R.layout.report_row, new String[]{"name", "age", "nic"}, new int[]{R.id.name, R.id.age, R.id.nic});
+        ListAdapter adapter = new SimpleAdapter(ViewAllReports.this, reportList, R.layout.report_row, new String[]{"name", "age", "nic", "_id"}, new int[]{R.id.name, R.id.age, R.id.nic, R.id.tv_idViewAllReports}){
+            public View getView (int position, View convertView, ViewGroup parent){
+                View v = super.getView(position, convertView, parent);
+                Button btn_view = (Button)v.findViewById(R.id.btn_viewViewAllReports);
+                TextView tv_idViewAllReports = (TextView)v.findViewById(R.id.tv_idViewAllReports);
+                int reportId = Integer.parseInt(tv_idViewAllReports.getText().toString());
+
+                btn_view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ViewAllReports.this, "Clicked" + reportId, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return v;
+            }
+        };
 
         lv_reportList.setAdapter(adapter);
 
