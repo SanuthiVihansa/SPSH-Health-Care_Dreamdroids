bcbdb0536334d2d961aebaea9b2e87d29b3a3f2e
it21134180 <it21134180@my.sliit.lk>
2022-08-26 19:50:41 +0530

    commit view a specific row

--- app/src/main/java/database/dbHelper.java
+++ app/src/main/java/database/dbHelper.java
@@ -1,8 +1,13 @@
 package database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class dbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="SPSH.db";
@@ -27,8 +32,85 @@ public class dbHelper extends SQLiteOpenHelper {
 
     }
 
+    public long addInfo (String itemCode, String itemName, String producerName, String usage, Integer strength, String expirationDate, String manufactureDate, Double unitPrice, String description ) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        //create a new map of values, where column names the key
+        ContentValues values = new ContentValues();
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMCODE, itemCode);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMNAME, itemName);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_PRODUCERNAME, producerName);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_USAGE, usage);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_STRENGTH, strength);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_EXPIRATIONDATE, expirationDate);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_MANUFACTURINGDATE, manufactureDate);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_UNITPRICE, unitPrice);
+                values.put(PharmacyMaster.Pharmacy.COLOUMN_NAME_DESCRIPTION, description);
+
+        return sqLiteDatabase.insert (PharmacyMaster.Pharmacy.TABLE_NAME,null,values);
+    }
+public List readAll(){
+        SQLiteDatabase db= getReadableDatabase();
+
+        String[] projection = {
+                PharmacyMaster.Pharmacy._ID,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMCODE,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMNAME,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_PRODUCERNAME,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_USAGE,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_STRENGTH,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_EXPIRATIONDATE,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_MANUFACTURINGDATE,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_UNITPRICE,
+                PharmacyMaster.Pharmacy.COLOUMN_NAME_DESCRIPTION
+        };
+
+        String sortOrder = PharmacyMaster.Pharmacy._ID+"DESC";
+        Cursor cursor = db.query(
+                PharmacyMaster.Pharmacy.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        List info = new ArrayList();
+        while (cursor.moveToNext()){
+            String itemCode = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMCODE));
+            String itemName = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMNAME));
+            String producerName = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_PRODUCERNAME));
+            String usage = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_USAGE));
+            String strength = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_STRENGTH));
+            String expirationDate = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_EXPIRATIONDATE));
+            String manufactureDate = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_MANUFACTURINGDATE));
+            String description = cursor.getString(cursor.getColumnIndexOrThrow(PharmacyMaster.Pharmacy.COLOUMN_NAME_USAGE));
+        }
+    cursor.close();
+    return info;
+}
+
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    //Delete
+    public void deleteInfo(String itemCode, String itemName, String producerName, String usage, Integer strength, String expirationDate, String manufactureDate, Double unitPrice, String description ){
+        //Use Readable to check if data is available --> "getReadableDATABASE"
+        SQLiteDatabase db = getReadableDatabase();
+
+        //Where we check if the value is there.
+        //Check if the username is equal or similar to the one that we are passing.
+        String selection = PharmacyMaster.Pharmacy.COLOUMN_NAME_ITEMCODE + "LIKE ?";
+
+
+        //Pass the columns as an array.
+        String[] stringArgs = {itemCode};
+
+        //delete the  query
+        db.delete((PharmacyMaster.Pharmacy.TABLE_NAME),selection,stringArgs);
 
     }
 }
