5380b50ae31c8859f8f34d889c3ef34d7399b058
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-27 21:44:13 +0530

    Modified UpdateReport.java file

--- app/src/main/java/com/spsh/spshhealthcare/UpdateReport.java
+++ app/src/main/java/com/spsh/spshhealthcare/UpdateReport.java
@@ -2,11 +2,15 @@ package com.spsh.spshhealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class UpdateReport extends AppCompatActivity {
+
+    int reportId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,12 @@ public class UpdateReport extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_update_report);
+
+        Intent intent = getIntent();
+        this.reportId = Integer.parseInt(intent.getStringExtra("reportId"));
+
+        EditText et_name = findViewById(R.id.et_labelForPatientNameUpdateReport);
+        et_name.setText(String.valueOf(this.reportId));
+
     }
 }
