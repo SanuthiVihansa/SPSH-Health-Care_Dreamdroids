59069882084eb80f26ff6a3f944b8c32e1be135f
it21134180 <it21134180@my.sliit.lk>
2022-09-05 22:02:34 +0530

    commiting the view all equipments page

---
+++ app/src/main/java/com/spsh/spshhealthcare/ViewAllPharamacyItems.java
@@ -0,0 +1,14 @@
+package com.spsh.spshhealthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllPharamacyItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_pharamacy_items);
+    }
+}
