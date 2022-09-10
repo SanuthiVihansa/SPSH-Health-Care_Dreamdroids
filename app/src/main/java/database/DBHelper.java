598a84d2e3cbd8f081f8b1c733065c17c07d8aca
IT21138386 <it21138386@my.sliit.lk>
2022-09-10 23:19:20 +0530

    implementation of CRUD and search functions in DBHelper.java file

--- app/src/main/java/database/DBHelper.java
+++ app/src/main/java/database/DBHelper.java
@@ -1,14 +1,17 @@
 package database;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.spsh.spshhealthcare.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,12 +43,10 @@ public class DBHelper extends SQLiteOpenHelper { //DBHelper class includes all t
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-    }
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {}
 
     //*************************************************************************Create method****************************************************************************
-    public long addInfo(String Pname, int age, String gender, String contactNo, String nic, String specialization, String doctorName, String date, String time){ //inside brackets we put elements we are going to pass
+    public long addAppointment(String Pname, int age, String gender, String contactNo, String nic, String specialization, String doctorName, String date, String time){ //inside brackets we put elements we are going to pass
         SQLiteDatabase db = getWritableDatabase();   //this create this db as an instance of Writeable Database so we will be able to add information to it
 
         //ContentValues class is used to create the object that will contain the username and the password, this will act as a row
@@ -66,29 +67,19 @@ public class DBHelper extends SQLiteOpenHelper { //DBHelper class includes all t
         return db.insert(AppointmentsMaster.Appointments.TABLE_NAME, null, values);
     }
 
-    //*************************************************************************Delete method****************************************************************************
-    public void deleteInfo(View view, String nic){
-        SQLiteDatabase db =  getReadableDatabase(); //this should be readable database because, we are going to check whether data is available when you are going to execute the query
-        String selection = AppointmentsMaster.Appointments.COLUMN_NAME_NIC + " LIKE ?"; //here we check username whether the username being passed to function(line 92) is same as the selection variable
-        String[] stringArgs = {nic};//passing arguments as arrays //you can delete multiple stuff if you add argument as an array
-
-        int count = db.delete(AppointmentsMaster.Appointments.TABLE_NAME,selection,stringArgs);
-
-        //displaying count using Snack bar
-        Snackbar snackbar = Snackbar.make(view, count + " rows affected",Snackbar.LENGTH_LONG);
-        snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
-        snackbar.show();
-    }
     //*************************************************************************Update method****************************************************************************
-    public void updateInfo(View view, String Pname, int age, String gender, String contactNo, String nic, String specialization, String doctorName, String date, String time){
+    public int updateAppointment(String Pname, int age, String gender, String contactNo, String id){
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues(); //creates an object that looks like a row
 
-        values.put(AppointmentsMaster.Appointments.COLUMN_NAME_CONTACTNO, contactNo);//in this case we are only changing the password so that is the only thing passed
+        values.put(AppointmentsMaster.Appointments.COLUMN_NAME_PNAME, Pname);//in this case we are only changing the password so that is the only thing passed
+        values.put(AppointmentsMaster.Appointments.COLUMN_NAME_AGE, age);
+        values.put(AppointmentsMaster.Appointments.COLUMN_NAME_GENDER, gender);
+        values.put(AppointmentsMaster.Appointments.COLUMN_NAME_CONTACTNO, contactNo);
 
-        String selection = AppointmentsMaster.Appointments.COLUMN_NAME_NIC + " LIKE ?"; //for the project we can use the id and access an entry to update whatever attributes(such as username and password) //When we pass arguments question mark will be replaced by the arguments
-        String[] selectionArgs = {nic};
+        String selection = AppointmentsMaster.Appointments._ID + " LIKE ?"; //for the project we can use the id and access an entry to update whatever attributes(such as username and password) //When we pass arguments question mark will be replaced by the arguments
+        String[] selectionArgs = {id};
 
         int count = db.update(
                 AppointmentsMaster.Appointments.TABLE_NAME,
@@ -96,23 +87,25 @@ public class DBHelper extends SQLiteOpenHelper { //DBHelper class includes all t
                 selection,
                 selectionArgs
         );
+        return count;
+    }
 
-        //displaying count using Snack bar
-        Snackbar snackbar = Snackbar.make(view, count + " rows affected",Snackbar.LENGTH_LONG); //the view being passed is the entire UI
-        snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
-        snackbar.show();
+    //*************************************************************************Delete method****************************************************************************
+    public void deleteAppointment(String appointmentID){
+        SQLiteDatabase db =  getReadableDatabase(); //this should be readable database because, we are going to check whether data is available when you are going to execute the query
+        String selection = AppointmentsMaster.Appointments._ID + " LIKE ?"; //here we check username whether the username being passed to function(line 92) is same as the selection variable
+        String[] stringArgs = {appointmentID};//passing arguments as arrays //you can delete multiple stuff if you add argument as an array
+
+        db.delete(AppointmentsMaster.Appointments.TABLE_NAME,selection,stringArgs);
     }
 
     //*************************************************************************Read method****************************************************************************
     @SuppressLint("Range")
-    public ArrayList<HashMap<String,String>> readAllInfo(String nic){
+    public ArrayList<HashMap<String,String>> readAllAppointments(String nic){
         SQLiteDatabase db = getReadableDatabase();
-
         ArrayList<HashMap<String,String>> allAppointments = new ArrayList<>();
-
         //SQL query
         String query = "SELECT "+AppointmentsMaster.Appointments._ID+", "+AppointmentsMaster.Appointments.COLUMN_NAME_PNAME+", "+AppointmentsMaster.Appointments.COLUMN_NAME_DATE+", "+AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME+" FROM "+AppointmentsMaster.Appointments.TABLE_NAME+" WHERE "+AppointmentsMaster.Appointments.COLUMN_NAME_NIC+"="+nic;
-
         Cursor cursor = db.rawQuery(query,null);
 
         while (cursor.moveToNext()){
@@ -125,7 +118,56 @@ public class DBHelper extends SQLiteOpenHelper { //DBHelper class includes all t
 
             allAppointments.add(hmap);
         }
-
         return allAppointments;
+    }
+
+    //*************************************************************************Read Single method****************************************************************************
+    @SuppressLint("Range")
+    public ArrayList readAppointmentByID(String id){
+        SQLiteDatabase db = getReadableDatabase();
+        ArrayList oneAppointment = new ArrayList();
+
+        //SQL Query
+        //or can use SELECT *
+        String query = "SELECT " +AppointmentsMaster.Appointments.COLUMN_NAME_PNAME+", "+AppointmentsMaster.Appointments.COLUMN_NAME_AGE+", "+AppointmentsMaster.Appointments.COLUMN_NAME_GENDER+", "+AppointmentsMaster.Appointments.COLUMN_NAME_CONTACTNO+", "+AppointmentsMaster.Appointments.COLUMN_NAME_NIC+", "+AppointmentsMaster.Appointments.COLUMN_NAME_SPECIALIZATION+ ", " +AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME+", "+AppointmentsMaster.Appointments.COLUMN_NAME_DATE+", "+AppointmentsMaster.Appointments.COLUMN_NAME_TIME+" FROM "+AppointmentsMaster.Appointments.TABLE_NAME+" WHERE "+AppointmentsMaster.Appointments._ID+"="+Integer.parseInt(id);
+
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        while (cursor.moveToNext()){
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_PNAME)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_AGE)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_GENDER)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_CONTACTNO)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_NIC)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_DATE)));
+            oneAppointment.add(cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_TIME)));
+        }
+        return oneAppointment;
+    }
+
+    //*************************************************************************Search method****************************************************************************
+    @SuppressLint("Range")
+    public ArrayList<HashMap<String,String>> searchAppointmentByName(String Pname){
+        SQLiteDatabase database = getReadableDatabase();
+        ArrayList<HashMap<String,String>> list = new ArrayList<>();
+
+        //SQL query
+        String query = "SELECT "+AppointmentsMaster.Appointments._ID+", "+AppointmentsMaster.Appointments.COLUMN_NAME_PNAME+", "+AppointmentsMaster.Appointments.COLUMN_NAME_DATE+", "+AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME+" FROM "+AppointmentsMaster.Appointments.TABLE_NAME+" WHERE "+AppointmentsMaster.Appointments.COLUMN_NAME_PNAME+"= '"+Pname + "'";
+
+        Cursor cursor = database.rawQuery(query,null);
+
+        while (cursor.moveToNext()){
+            HashMap<String , String> hmap = new HashMap<>();//this gives 1 appointment
+            //1st parameter                                                             //value from cursor
+            hmap.put(AppointmentsMaster.Appointments._ID,cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments._ID)));
+            hmap.put(AppointmentsMaster.Appointments.COLUMN_NAME_PNAME,cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_PNAME)));
+            hmap.put(AppointmentsMaster.Appointments.COLUMN_NAME_DATE,cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_DATE)));
+            hmap.put(AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME,cursor.getString(cursor.getColumnIndex(AppointmentsMaster.Appointments.COLUMN_NAME_DOCTORSNAME)));
+
+            list.add(hmap);
+        }
+        return list;
     }
 }
