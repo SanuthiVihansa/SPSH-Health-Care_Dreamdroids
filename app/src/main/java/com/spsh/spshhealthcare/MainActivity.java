151da8c4342e8d943698d0d8957fc1a3ab697deb
IT21138386 <it21138386@my.sliit.lk>
2022-09-18 19:54:35 +0530

    additional files

--- app/src/main/java/com/spsh/spshhealthcare/MainActivity.java
+++ app/src/main/java/com/spsh/spshhealthcare/MainActivity.java
@@ -3,6 +3,7 @@ package com.spsh.spshhealthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
