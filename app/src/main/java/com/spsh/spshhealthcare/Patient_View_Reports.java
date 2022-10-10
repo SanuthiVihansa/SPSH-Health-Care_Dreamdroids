75628891782bd6f15a740315855751891c3f8f4a
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-10-10 19:47:48 +0530

    Added Patient_View_Reports activity

---
+++ app/src/main/java/com/spsh/spshhealthcare/Patient_View_Reports.java
@@ -0,0 +1,14 @@
+package com.spsh.spshhealthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Patient_View_Reports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_view_reports);
+    }
+}
