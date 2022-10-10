05db1f494905f0fe5948cb572c7c9056f293109d
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-10-10 19:48:19 +0530

    Added properties to Patient_Report.java

--- app/src/main/java/com/spsh/spshhealthcare/Patient_Report.java
+++ app/src/main/java/com/spsh/spshhealthcare/Patient_Report.java
@@ -2,13 +2,18 @@ package com.spsh.spshhealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Patient_Report extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_patient_report);
     }
 }
