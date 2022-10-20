76abe30b7fc02b23c80ec4e269607f41c6d835a5
IT21138386 <it21138386@my.sliit.lk>
2022-10-20 17:25:48 +0530

    formatted code

--- app/src/main/java/com/spsh/spshhealthcare/Patient_All_Appointments.java
+++ app/src/main/java/com/spsh/spshhealthcare/Patient_All_Appointments.java
@@ -52,8 +52,8 @@ public class Patient_All_Appointments extends AppCompatActivity {
         ListAdapter listAdapter = new SimpleAdapter(Patient_All_Appointments.this,allAppointments,R.layout.appointment_row,new String[]{"Pname","date","doctorName","_id"},new int[]{R.id.tv_row_name_sathira,R.id.tv_row_date_sathira,R.id.tv_row_dName_sathira,R.id.tv_single_appointment_sathira}){
             public View getView(int position, View convertView, ViewGroup parent){ //fetches a view
                 View view = super.getView(position, convertView, parent);
-                Button button = (Button) view.findViewById(R.id.btn_row_view_sathira);//this is the "view button" from wireframe
-                TextView textView = (TextView) view.findViewById(R.id.tv_single_appointment_sathira);    //fetches id of invisible textbox
+                Button button = (Button) view.findViewById(R.id.btn_row_view_sathira);  //this is the "view button" from wireframe
+                TextView textView = (TextView) view.findViewById(R.id.tv_single_appointment_sathira);   //fetches id of invisible textbox
                 String appointmentID = textView.getText().toString();
 
                 button.setOnClickListener(new View.OnClickListener() {
