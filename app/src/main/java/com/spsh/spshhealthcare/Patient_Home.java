9199bae7e3ffec5d5f88e250bcc1123092b61105
IT21138386 <it21138386@my.sliit.lk>
2022-10-21 18:23:05 +0530

    indented code

--- app/src/main/java/com/spsh/spshhealthcare/Patient_Home.java
+++ app/src/main/java/com/spsh/spshhealthcare/Patient_Home.java
@@ -11,12 +11,13 @@ import android.widget.Button;
 
 public class Patient_Home extends AppCompatActivity {
 
-//    Button button; //part of creating intent
+    //    Button button; //part of creating intent
     private String nic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Full screen and orientation code
         getSupportActionBar().hide();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -26,32 +27,23 @@ public class Patient_Home extends AppCompatActivity {
 
         Intent intent = getIntent();
         this.nic = intent.getStringExtra("nic");
-
-//        //Method 1 to create intents
-//        button = findViewById(R.id.btn_viewAppoint_sathira);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(Patient_Home.this,Patient_All_Appointments.class);
-//                startActivity(intent);
-//            }
-//        });
     }
+
     //Method 2 to create intents
-    public void onClickViewAppointmentBtn(View view){
-        Intent intent = new Intent(this,Patient_All_Appointments.class);
+    public void onClickViewAppointmentBtn(View view) {
+        Intent intent = new Intent(this, Patient_All_Appointments.class);
         intent.putExtra("nic", this.nic);
         startActivity(intent);
     }
 
-    public void onClickMakeAppointmentBtn(View view){
-        Intent intent = new Intent(this,Patient_Add.class);
+    public void onClickMakeAppointmentBtn(View view) {
+        Intent intent = new Intent(this, Patient_Add.class);
         intent.putExtra("nic", this.nic);
         startActivity(intent);
     }
 
-    public void onClickBackBtn(View view){
-        Intent intent = new Intent(this,MainActivity.class);
+    public void onClickBackBtn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
